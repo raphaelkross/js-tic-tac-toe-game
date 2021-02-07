c0b5b605f5a04c6682da9caa6a21bbcbf8b6f85e
Rafael Angeline <xiaothemes@gmail.com>
2021-02-07 15:44:06 -0300

    Add winner check logic and tests

---
+++ api/helpers/game.js
@@ -0,0 +1,84 @@
+/**
+ * Game Helpers.
+ */
+
+const { check } = require("prettier");
+
+const checkWinner = (player, game) => {
+  // Convert array to matrix.
+  let matrix = [];
+
+  const gameMod = game.slice(0, game.length);
+
+  for (let i = 3; i > 0; i--) {
+    matrix.push(gameMod.splice(0, Math.ceil(gameMod.length / i)));
+  }
+
+  /*
+    [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8]
+    ]
+  */
+
+  // Check if there's horizontal winner.
+  for (let row = 0; row < 3; row++) {
+    if (
+      matrix[row][0] == player &&
+      matrix[row][1] == player &&
+      matrix[row][2] == player
+    ) {
+      return true;
+    }
+  }
+
+  // Check if there's vertical winner.
+  for (let col = 0; col < 3; col++) {
+    if (
+      matrix[0][col] == player &&
+      matrix[1][col] == player &&
+      matrix[2][col] == player
+    ) {
+      return true;
+    }
+  }
+
+  // Check diagonals.
+  if (
+    (matrix[0][0] == player &&
+      matrix[1][1] == player &&
+      matrix[2][2] == player) ||
+    (matrix[0][2] == player && matrix[1][1] == player && matrix[2][0] == player)
+  ) {
+    return true;
+  }
+
+  return false;
+};
+
+module.exports = {
+  getGameStatus: (game) => {
+    // Check if empty cells on the game.
+    const isThereEmptyCells = game.find((cell) => cell === "");
+
+    // There's still game to go...
+    if (isThereEmptyCells === "") {
+      return "";
+    }
+
+    const isXWinner = checkWinner("X", game);
+    const isOWinner = checkWinner("O", game);
+
+    if (isXWinner) {
+      return "X";
+    }
+
+    if (isOWinner) {
+      return "O";
+    }
+
+    // Tie.
+    return "-";
+  },
+};
---
+++ api/helpers/game.test.js
@@ -0,0 +1,37 @@
+const GameHelpers = require("./game");
+
+describe("Game Helpers", () => {
+  it("should return false if game is not complete", () => {
+    // ["", "", "", "", "", "", "", "", ""]
+    const game = ["", "", "X", "", "O", "", "", "", ""];
+    expect(GameHelpers.getGameStatus(game)).toBe("");
+  });
+
+  it("should return [Player] if game is horizontal won by [Player]", () => {
+    const game = ["X", "X", "X", "O", "X", "O", "O", "O", "X"];
+    const game2 = ["X", "O", "O", "X", "X", "X", "O", "X", "O"];
+    const game3 = ["X", "X", "O", "O", "O", "X", "X", "X", "X"];
+
+    expect(GameHelpers.getGameStatus(game)).toBe("X");
+    expect(GameHelpers.getGameStatus(game2)).toBe("X");
+    expect(GameHelpers.getGameStatus(game3)).toBe("X");
+  });
+  it("should return [Player] if game is vertically won by [Player]", () => {
+    const game = ["O", "X", "O", "O", "X", "O", "O", "O", "X"];
+    const game2 = ["O", "X", "O", "O", "X", "X", "X", "X", "O"];
+    const game3 = ["O", "O", "X", "X", "O", "X", "O", "X", "X"];
+    expect(GameHelpers.getGameStatus(game)).toBe("O");
+    expect(GameHelpers.getGameStatus(game2)).toBe("X");
+    expect(GameHelpers.getGameStatus(game3)).toBe("X");
+  });
+  it("should return [Player] if game is diagonal won by [Player]", () => {
+    const game = ["X", "O", "O", "X", "X", "O", "O", "O", "X"];
+    const game2 = ["X", "X", "O", "X", "O", "X", "O", "X", "X"];
+    expect(GameHelpers.getGameStatus(game)).toBe("X");
+    expect(GameHelpers.getGameStatus(game2)).toBe("O");
+  });
+  it("should return - if game is tied", () => {
+    const game = ["X", "O", "O", "O", "X", "X", "O", "X", "O"];
+    expect(GameHelpers.getGameStatus(game)).toBe("-");
+  });
+});
